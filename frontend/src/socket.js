bdba120c49ea740d8ccb7fd935a1277dd7f2e4b0
Harsha Vardhan Reddy <23eg105e34@anurag.edu.in>
2026-05-28 17:31:08 +0530

    forced websocket transport

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,9 +1,12 @@
 import { io }
 from "socket.io-client";
 
-
-const socket =
-  io("https://society-management-app-production-3b8b.up.railway.app");
-
+const socket = io(
+  "https://society-management-app-production-3b8b.up.railway.app",
+  {
+    transports: ["websocket"],
+    withCredentials: true,
+  }
+);
 
 export default socket;
