ebe68f7b60f157c7e0c18dd283da36a362692240
Harshavardhan Reddy <23eg105e34@anurag.edu.in>
2026-06-11 10:38:49 +0530

    Update axios.jsx

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://society-management-app-production-3b8b.up.railway.app/api",
+    "https://society-management-app-production-cd99.up.railway.app/api",
 });
 
 // attach token automatically
 API.interceptors.request.use((req) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
